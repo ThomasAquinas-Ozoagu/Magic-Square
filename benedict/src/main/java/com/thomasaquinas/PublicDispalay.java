d918cce4f0df83a5fd975b9c1aab26c3e190bbb5
OZOAGU, ThomasAquinas Benedict <ozoagu.benedict@gmail.com>
2019-07-24 06:02:49 +0100

    The first implementation

---
+++ benedict/src/main/java/com/thomasaquinas/PublicDispalay.java
@@ -0,0 +1,19 @@
+package com.thomasaquinas;
+
+/**
+ * Created by Ozoagu ThomasAquinas on 7/2/2019.
+ */
+
+class PublicDisplay {
+    public void matrix3x3(int j, int k, int l, int m, int n, int o, int p, int q, int r, int s){
+        System.out.println("Result No "+j+" is ");
+        System.out.println("- - - - - - -");
+        System.out.println("|  "+k+"  "+l+"  "+m+"  |");
+        System.out.println("|           |");
+        System.out.println("|  "+n+"  "+o+"  "+p+"  |");
+        System.out.println("|           |");
+        System.out.println("|  "+q+"  "+r+"  "+s+"  |");
+        System.out.println("- - - - - - -");
+        System.out.println();
+    }
+}
